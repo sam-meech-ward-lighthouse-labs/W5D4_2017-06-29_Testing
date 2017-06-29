643c9256ee7d4326fced549b32d3f7e4b02b16b6
Sam Meech-Ward <sam@meech-ward.com>
2017-06-29 09:21:08 -0700

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Sam Meech-Ward on 2017-06-29.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
